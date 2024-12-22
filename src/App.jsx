bd65480c1d5731704d69d16bf611451ee6c75c04
MShubar <pokeeeeepow@gmail.com>
2024-12-22 18:29:36 +0300

    just first one

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Create from './pages/Create'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Create />
+    </>
+  )
+}
+
+export default App
---
+++ src/services/petService.js
@@ -0,0 +1,5 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/pets`
+
+const petService = () => {}
+
+export default petService
